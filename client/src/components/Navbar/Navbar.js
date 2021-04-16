b379f43e6c84647abc711c121d364cd2a8193659
Kenneth Dy <kennethndy@yahoo.com>
2021-04-16 02:19:26 -0700

    added pages and navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    marginRight: theme.spacing(2),
+    textDecoration: 'none',
+    color: 'inherit'
+  }
+}))
+
+const Navbar = () => {
+
+  const classes = useStyles()
+  
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+       
+          <Typography variant="h6" className={classes.title}>
+            | Google Book Search |
+          </Typography>
+          <Link to='/' className={classes.link}>
+            <Button color='inherit'>Search</Button>
+          </Link>
+          <Link to='/saved' className={classes.link}>
+            <Button color='inherit'>Saved</Button>
+          </Link>
+         
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
